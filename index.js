39f6984bc61ecdc8c1d41fb2d3b0d2ec9f27a2d7
darshan sutariya <pateldarshan1322005@gmail.com>
2024-07-24 22:13:49 +0530

    Port

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const {MongoClient} = require('mongodb');
 let url = "mongodb://localhost:27017/";
 let connect = new MongoClient(url);
+require('dotenv');
 
 app.use(express.json());
 
@@ -13,7 +14,7 @@ app.post('/',async(req,res)=>
     let result =await collection.insertOne(req.body);
     res.send(result);
 });
-app.listen(2200,()=>
+app.listen(process.env.PORT||2000,()=>
 {
     console.log("App Active");
 });
